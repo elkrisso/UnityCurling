cbbd451c5af5bf54ac91a6fa06b49064fabee885
kriss <kristof.kind@monkey-works.de>
2018-11-14 15:43:28 +0100

    added basic scene, and functionality

---
+++ Curling/Assets/MovingObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+    private bool pressed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // we don't need that right now
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveHorizontal = 0.0f;
+        float moveVertical = 3.0f;
+
+        if (Input.GetKey("space"))
+        {
+            Debug.Log("space pressed!");
+            if (speed >= 400)
+            {
+                //wait for key being released
+                speed = 0;
+            }
+            else {
+                speed = speed + 2f;
+            }
+        }
+
+        if (Input.GetKeyUp("space"))
+        {
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            rb.AddForce(movement * speed);
+            speed = 0f;
+        }
+
+    }
+}

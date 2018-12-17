ef5046a74772e0dbe856cded172fb4585a9437cd
kriss <kristof.kind@monkey-works.de>
2018-12-17 21:43:41 +0100

    added background,powerbar, not falling down anymore and spot light

---
+++ Curling/Assets/PowerBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerBar : MonoBehaviour {
+    [SerializeField]  public Image image;
+    public MovingObject powerObject;
+    private Mask mask;
+	// Use this for initialization
+	void Start () {
+        image = GetComponent<Image>();
+        image.type = Image.Type.Filled;
+        image.fillAmount = 50;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        image.fillAmount = powerObject.speed / 100;
+	}
+}

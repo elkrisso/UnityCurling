1cb72ffdce7c59d74387c947a6344e627a1e1273
Philippé Corleone <Corleone@ipc13263-4.local>
2018-12-19 12:37:04 +0100

    add: spotlight1 (it folows the stone)

---
+++ Curling/Assets/Spotlight1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spotlight1 : MonoBehaviour {
+
+    public Transform target;
+    public float smoothSpeed = 0.005f;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        Vector3 velocity = new Vector3(0, 0, 0);
+        // Smoothly move the camera towards that target position
+        transform.position = Vector3.SmoothDamp(transform.position, target.position + offset, ref velocity, smoothSpeed);
+    }
+}

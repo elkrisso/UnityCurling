5f48434ce7d8c360cf67136987d4f4ee11c2c5d3
Philippé Corleone <Corleone@ipc13263-4.local>
2019-01-09 17:04:42 +0100

    add light

--- Curling/Assets/Spotlight1.cs
+++ Curling/Assets/Spotlight1.cs
@@ -13,5 +13,6 @@ public class Spotlight1 : MonoBehaviour {
         Vector3 velocity = new Vector3(0, 0, 0);
         // Smoothly move the camera towards that target position
         transform.position = Vector3.SmoothDamp(transform.position, target.position + offset, ref velocity, smoothSpeed);
+        transform.GetComponent<Light>().color = new Color(255f * (float)target.position.z / 100f, 0f, 0f);
     }
 }
